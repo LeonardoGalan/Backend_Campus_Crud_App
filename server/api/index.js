067ab4ad0a032135e0d849241d0c04d78f40dd8d
LeonardoGalan <leonardogalan2@gmail.com>
2022-01-22 18:27:02 -0500

    heroku test #10000

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,9 @@ const Router = require('express').Router()
 
 Router.use('/students', require('./students'))
 Router.use('/', require('./campuses'))
-
-
-// Router.get('/', (req,res)=>{
-//     try {res.send("Welcome to the API level!")}
-//     catch(error){res.send(error.message)}
-// })
+Router.get('/', (req,res)=>{
+    try {res.send("Welcome to the API level!")}
+    catch(error){res.send(error.message)}
+})
 
 module.exports = Router
