664a0d249afb06a65a94fc05f5a421ac123877b3
LeonardoGalan <leonardogalan2@gmail.com>
2022-01-22 17:55:06 -0500

    testtt333333t3333asdasd

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,12 @@
 const Router = require('express').Router()
 
 Router.use('/students', require('./students'))
-Router.use('/campuses', require('./campuses'))
-Router.get('/', (req,res)=>{
-    try {res.send("Welcome to the API level!")}
-    catch(error){res.send(error.message)}
-})
+Router.use('/', require('./campuses'))
+
+
+// Router.get('/', (req,res)=>{
+//     try {res.send("Welcome to the API level!")}
+//     catch(error){res.send(error.message)}
+// })
+
 module.exports = Router
