4e4a22e683d9422568418ee884c0c5196380b8ca
LeonardoGalan <leonardogalan2@gmail.com>
2022-01-22 23:34:04 -0500

    this might work 13

--- server/database/database.js
+++ server/database/database.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize')
 // const myDatabase = new Sequelize("postgres://postgres:123@localhost:5432/campusdb",{logging : false})
 //You have to create a database on PSQL named 'campusdb' 
 //and set the password field to whatever your password is
-const myDatabase = new Sequelize("postgres://postgres:123@localhost:5432/campusdb", {
+const myDatabase = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:123@localhost:5432/campusdb", {
   
 //   host: "xxxxxx.eu-west-1.compute.amazonaws.com",
 // dialect: 'postgres',
